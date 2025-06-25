d5e036128765eedfa2a5e50aa15103cdb64c3204
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-06-25 21:32:02 +0400

    fix: header title and its size

--- src/components/module/homePage/HeroSection.tsx
+++ src/components/module/homePage/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = ({ data }: { data: IHeroSection[] }) => {
   return (
     <div className="relative h-[80vh] flex items-center justify-center overflow-hidden">
       <div
-        className="absolute inset-0 bg-cover w-full bg-center blur-sm"
+        className="absolute inset-0 bg-cover w-full bg-center blur-sm "
         style={{ backgroundImage: `url(${data[0]?.backgroundImage})` }}
       ></div>
 
@@ -19,10 +19,10 @@ const HeroSection = ({ data }: { data: IHeroSection[] }) => {
         transition={{ duration: 0.8, ease: "easeOut" }}
         className="relative z-10 w-[600px] mx-auto text-center p-8 rounded-xl "
       >
-        <h1 className="text-[45px] md:text-6xl font-bold mb-4 leading-10 md:leading-14 tracking-wide">
+        <h1 className="text-[45px] text-white md:text-6xl font-bold mb-4 leading-10 md:leading-14 tracking-wide">
           {data[0]?.title}
         </h1>
-        <p className="text-gray-700 mb-4">{data[0]?.subTitle}</p>
+        <p className="text-gray-200 mb-4">{data[0]?.subTitle}</p>
       </motion.div>
     </div>
   );
